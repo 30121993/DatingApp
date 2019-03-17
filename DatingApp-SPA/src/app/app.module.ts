27e719afb5d49d9a654d010963330dea7ff0375c
Trịnh duy Long <longtd@vtechno.com.vn>
2019-03-17 21:20:54 +0700

    43. Parent to Child Component communication using Input properties 0

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,34 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
-import { HttpClient } from 'selenium-webdriver/http';
-import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from "./app.component";
+import { ValueComponent } from "./value/value.component";
+import { NavComponent } from "./nav/nav.component";
+import { AuthService } from "./_services/auth.service";
+import { RegisterComponent } from "./register/register.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      ValueComponent,
-      NavComponent,
-      RegisterComponent,
-      HomeComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      FormsModule
-   ],
-   providers: [
-      AuthService
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    ValueComponent,
+    NavComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
