1da1163489884ad625b26553c02a07856b5767a0
Trịnh duy Long <longtd@vtechno.com.vn>
2019-03-27 22:57:15 +0700

    63. Protecting our routes with a route guard

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { HomeComponent } from "./home/home.component";
 import { MemberListComponent } from "./member-list/member-list.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { ListsComponent } from "./lists/lists.component";
+import { AuthGuard } from "./_guards/auth.guard";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
-  { path: "member", component: MemberListComponent },
-  { path: "messages", component: MessagesComponent },
-  { path: "lists", component: ListsComponent },
+  { path: "members", component: MemberListComponent, canActivate: [AuthGuard] },
+  { path: "messages", component: MessagesComponent, canActivate: [AuthGuard] },
+  { path: "lists", component: ListsComponent, canActivate: [AuthGuard] },
   { path: "**", redirectTo: "home", pathMatch: "full" }
 ];
 
