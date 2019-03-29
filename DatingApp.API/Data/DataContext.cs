7f7dce7ae044cf017fbea0d697a35dfab05fec6b
Trịnh duy Long <longtd@vtechno.com.vn>
2019-03-29 22:33:02 +0700

    67. Extending the User Model

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -4,10 +4,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.Data
 {
-    public class DataContext : DbContext
-    {
-        public DataContext(DbContextOptions<DataContext> options) : base (options)  { }
-        public DbSet<Value> Values { get; set; }
-        public DbSet<User> Users { get; set; }
-    }
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    public DbSet<Value> Values { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public DbSet<Photo> Photos { get; set; }
+  }
 }
