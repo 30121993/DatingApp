5d9e7536be0c42aef58d1b3708840f6759f6b9a7
Trịnh duy Long <longtd@vtechno.com.vn>
2019-04-16 21:04:51 +0700

    72. Creating a new repository for our API

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -37,6 +37,7 @@ namespace DatingApp
       services.AddCors();
       services.AddTransient<Seed>();
       services.AddScoped<IAuthRepository, AuthRepository>();
+      services.AddScoped<IDatingRepository, DatingRepository>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
