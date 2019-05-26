45b4d0fccdd9939542f1ad035494e5d588f2d1e5
longtd <longtd3012@gmail.com>
2019-05-26 13:17:17 +0700

    97. Adding a CanDeactivate route guard

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
 import { MemberListResolver } from "./_resolvers/member-list.resolver";
 import { MemberEditComponent } from "./members/member-edit/member-edit.component";
 import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
+import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
@@ -34,7 +35,8 @@ const routes: Routes = [
       {
         path: "member/edit",
         component: MemberEditComponent,
-        resolve: { user: MemberEditResolver }
+        resolve: { user: MemberEditResolver },
+        canDeactivate: [PreventUnsavedChanges]
       },
       { path: "messages", component: MessagesComponent },
       { path: "lists", component: ListsComponent }
